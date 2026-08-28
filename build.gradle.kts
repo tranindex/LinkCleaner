998d0ae60424a6d5e7aa4d31bdf2459d699bbfe8
tranindex <zhengfaming1819264676@gmail.com>
2026-08-28 21:07:39 +0800

    4- version1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.jetbrainsCompose) apply false
-    alias(libs.plugins.compose.compiler) apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "1.9.22" apply false
+    id("org.jetbrains.compose") version "1.6.10" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
 }
--- composeApp/src/desktopMain/kotlin/com/linkcleaner/app/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/linkcleaner/app/Main.kt
@@ -2,10 +2,11 @@ package com.linkcleaner.app
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import kotlin.system.exitProcess
 
 fun main() = application {
     Window(
-        onCloseRequest = ::exitApplication,
+        onCloseRequest = { exitProcess(0) },
         title = "Link Cleaner"
     ) {
         App()
